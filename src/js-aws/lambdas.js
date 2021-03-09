2e0d8a14081a7eca53f75519c94cb567c9d1c170
Casey Marshall <casey@metastatic.org>
2021-03-08 20:11:22 -0800

    Lambda updates.

--- src/js-aws/lambdas.js
+++ src/js-aws/lambdas.js
@@ -23,7 +23,7 @@ async function authenticate(event, context) {
     }
     token = token.substr(7);
     if (jws.verify(token, 'ES256', publicKey)) {
-        let claims = jws.decode(token).payload;
+        let claims = JSON.parse(jws.decode(token).payload);
         if (claims.exp == null || claims.exp < Date.now() / 1000 || claims.sub == null) {
             return {isAuthorized: false};
         } else {
@@ -45,15 +45,14 @@ async function login(event, context) {
     if (dynamo == null) {
         dynamo = new AWS.DynamoDB.DocumentClient();
     }
-    let queryResult = await dynamo.scan({
+    let queryResult = await dynamo.get({
         TableName: process.env.USERS_DYNAMODB_TABLE,
-        IndexName: 'emailHash-index',
-        ExclusiveStartKey: emailHash,
+        Key: {emailHash},
     }).promise();
-    if (queryResult.Items.length == 0) {
+    if (queryResult.Item == null) {
         return {statusCode: 401, body: JSON.stringify({error: 'Invalid credentials'})};
     }
-    let userInfo = queryResult.items[0];
+    let userInfo = queryResult.Item;
     let computedHash = crypto.scryptSync(params.password, userInfo.salt, 64);
     if (userInfo.passwordHash != computedHash.toString('hex')) {
         return {statusCode: 401, body: JSON.stringify({error: 'Invalid credentials'})};
@@ -92,12 +91,54 @@ async function login(event, context) {
     };
 }
 
-async function getSignedUrl(event, context) {
-    let uuid = context.authorizer.uuid;
+function getSignedUrl(s3client, op, args) {
+    return new Promise((resolve, reject) => {
+        s3client.getSignedUrl(op, args, (err, url) => {
+            if (err != null) {
+                reject(err);
+            } else {
+                resolve(url);
+            }
+        })
+    })
+}
+
+async function getSignedUrls(event, context) {
+    let uuid = event.requestContext.authorizer.lambda.uuid;
     if (uuid == null) {
         return {statusCode: 401, body: JSON.stringify({error: 'Unauthorized'})};
     }
     if (s3 == null) {
         s3 = new AWS.S3();
     }
+    let getKeyUrl = await getSignedUrl(s3, 'getObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: `athens/${uuid}/.key`,
+    });
+    let putKeyUrl = await getSignedUrl(s3, 'putObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: `athens/${uuid}/.key`,
+    });
+    let getDataUrl = await getSignedUrl(s3, 'getObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: `athens/${uuid}/index.transit`,
+    });
+    let putDataUrl = await getSignedUrl(s3, 'putObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: `athens/${uuid}/index.transit`
+    });
+    return {
+        statusCode: 200,
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({
+            getKeyUrl,
+            putKeyUrl,
+            getDataUrl,
+            putDataUrl,
+        })
+    };
 }
+
+module.exports.authenticate = authenticate;
+module.exports.login = login;
+module.exports.getSignedUrls = getSignedUrls;
