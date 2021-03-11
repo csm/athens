7e5bd4adfc4e0d6a5cffbfcf742f90a457fbeeab
Casey Marshall <casey@metastatic.org>
2021-03-10 23:14:11 -0800

    Reorganize AWS code.

--- aws/index.js
+++ aws/index.js
@@ -35,9 +35,22 @@ async function authenticate(event, context) {
 }
 
 async function login(event, context) {
-    let params = JSON.parse(event.body);
+    var params;
+    try {
+        params = JSON.parse(event.body);
+    } catch (e) {
+        return {
+            statusCode: 400,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'invalid request'});
+        }
+    }
     if (params.email == null || params.password == null) {
-        return {statusCode: 400, body: JSON.stringify({error: 'Invalid request'})};
+        return {
+            statusCode: 400,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'Invalid request'})
+        };
     }
     let hash = crypto.createHash('sha256');
     hash.update(params.email.toLowerCase());
@@ -50,15 +63,27 @@ async function login(event, context) {
         Key: {emailHash},
     }).promise();
     if (queryResult.Item == null) {
-        return {statusCode: 401, body: JSON.stringify({error: 'Invalid credentials'})};
+        return {
+            statusCode: 401,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'Invalid credentials'})
+        };
     }
     let userInfo = queryResult.Item;
     let computedHash = crypto.scryptSync(params.password, userInfo.salt, 64);
     if (userInfo.passwordHash != computedHash.toString('hex')) {
-        return {statusCode: 401, body: JSON.stringify({error: 'Invalid credentials'})};
+        return {
+            statusCode: 401,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'Invalid credentials'})
+        };
     }
     if (userInfo.expires != null && userInfo.expires < Date.now()) {
-        return {statusCode: 401, body: JSON.stringify({error: 'Account expired'})};
+        return {
+            statusCode: 401,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'Account expired'})
+        };
     }
     if (privateKey == null) {
         if (ssm == null) {
@@ -84,6 +109,7 @@ async function login(event, context) {
     });
     return {
         statusCode: 200,
+        headers: {'Content-type': 'application/json'},
         body: JSON.stringify({
             token: token,
             uuid: userInfo.uuid,
@@ -91,7 +117,7 @@ async function login(event, context) {
     };
 }
 
-function getSignedUrl(s3client, op, args) {
+function _getSignedUrl(s3client, op, args) {
     return new Promise((resolve, reject) => {
         s3client.getSignedUrl(op, args, (err, url) => {
             if (err != null) {
@@ -103,39 +129,42 @@ function getSignedUrl(s3client, op, args) {
     })
 }
 
-async function getSignedUrls(event, context) {
+async function getSignedUrl(event, context) {
     let uuid = event.requestContext.authorizer.lambda.uuid;
     if (uuid == null) {
-        return {statusCode: 401, body: JSON.stringify({error: 'Unauthorized'})};
+        return {
+            statusCode: 401,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'Unauthorized'})
+        };
+    }
+    let path = event.queryStringParameters.path;
+    if (path == null || path.indexOf('/') >= 0) {
+        return {
+            statusCode: 400,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'invalid path'})
+        };
+    }
+    let verb = event.queryStringParameters.verb;
+    if (verb == null || (verb != 'get' && verb != 'put')) {
+        return {
+            statusCode: 400,
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({error: 'invalid verb (should be get or put)'})
+        }
     }
     if (s3 == null) {
         s3 = new AWS.S3();
     }
-    let getKeyUrl = await getSignedUrl(s3, 'getObject', {
+    let url = await _getSignedUrl(s3, verb == 'get' ? 'getObject' : 'putObject', {
         Bucket: process.env.S3_BUCKET,
-        Key: `athens/${uuid}/.key`,
-    });
-    let putKeyUrl = await getSignedUrl(s3, 'putObject', {
-        Bucket: process.env.S3_BUCKET,
-        Key: `athens/${uuid}/.key`,
-    });
-    let getDataUrl = await getSignedUrl(s3, 'getObject', {
-        Bucket: process.env.S3_BUCKET,
-        Key: `athens/${uuid}/index.transit`,
-    });
-    let putDataUrl = await getSignedUrl(s3, 'putObject', {
-        Bucket: process.env.S3_BUCKET,
-        Key: `athens/${uuid}/index.transit`
+        Key: `athens/${uuid}/${path}`
     });
     return {
         statusCode: 200,
         headers: {"content-type": "application/json"},
-        body: JSON.stringify({
-            getKeyUrl,
-            putKeyUrl,
-            getDataUrl,
-            putDataUrl,
-        })
+        body: JSON.stringify({url})
     };
 }
 
